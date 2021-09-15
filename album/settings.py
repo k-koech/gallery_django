7df27e330cc4c3c3af1eba463edda1612f726c8f
k-koech <kelvin.koech@student.moringaschool.com>
2021-09-16 00:34:32 +0300

    updated allowed hosts in  settings

--- album/settings.py
+++ album/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = os.environ['DEBUG']
 # ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
 
 
 
